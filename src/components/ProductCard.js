3cbd33e0298490e9d0e3c7b5d63fb176d470bfe6
nanong11 <marcallen.nanong11@gmail.com>
2022-04-01 18:31:17 +0800

    Modified productCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -8,14 +8,17 @@ export default function ProductCard({productProp}) {
   return (
     <Grid item>
       <Card 
-      sx={{m: 3, maxWidth: 350, height: 650, display: "flex", flexDirection: "column", justifyContent: "space-between"}}
+      sx={{m: 3, maxWidth: 350, height: 650, display: "flex", flexDirection: "column"}}
       >
         <CardMedia
           component="img"
           image="/img/B450.jpg"
           sx={{ padding: 2}}
         />
-        <CardContent>
+        
+        <CardContent
+        sx={{flexGrow: 1}}
+        >
           <Typography
             variant="h6"
             component="h2"
@@ -28,20 +31,26 @@ export default function ProductCard({productProp}) {
           >
             {description}
           </Typography>
+          
+        </CardContent>
+
+        <CardContent>
           <Typography
-            sx={{mt: 1, fontSize: "1.2rem", fontWeight: 700 }}
+            component="div"
+            sx={{fontSize: "1.2rem", fontWeight: 700 }}
             color="primary"
           >
             ${price}
           </Typography>
         </CardContent>
+        
         <CardActions
-        sx={{paddingLeft: "1rem", paddingRight: "1rem"}}
+          sx={{paddingLeft: "1rem", paddingRight: "1rem"}}
         >
-            <Button variant='contained' size='small' sx={{mr: "auto"}}>View</Button>
-            <IconButton color='primary'><RemoveCircleOutlineIcon fontSize="large" /></IconButton>
-            <Typography sx={{m: "auto .5rem"}}>1</Typography>
-            <IconButton color='primary'><AddCircleOutlineIcon fontSize="large" /></IconButton>
+          <Button variant='contained' size='small' sx={{mr: "auto"}}>View</Button>
+          <IconButton color='primary'><RemoveCircleOutlineIcon fontSize="large" /></IconButton>
+          <Typography sx={{m: "auto .5rem"}}>1</Typography>
+          <IconButton color='primary'><AddCircleOutlineIcon fontSize="large" /></IconButton>
         </CardActions>
       </Card>
     </Grid>
